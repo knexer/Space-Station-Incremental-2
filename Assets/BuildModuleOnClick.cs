afb701f13358228c7e8a6768cf1ed646ec0f80c2
knexer <knexer@gmail.com>
2019-02-17 17:04:46 -0800

    Can build crew modules on vertebra leaves

---
+++ Assets/BuildModuleOnClick.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildModuleOnClick : MonoBehaviour
+{
+    [SerializeField] private GameObject module;
+
+    private void OnMouseDown()
+    {
+        GetComponent<BoxCollider2D>().enabled = false;
+        GetComponent<SpriteRenderer>().enabled = false;
+        // TODO spawn a module picker GUI or something once there are multiple modules
+        Instantiate(module, transform);
+    }
+}
