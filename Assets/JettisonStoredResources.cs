2c0172ebd57ec06cb9a82b1d7d9ea77729516ba3
knexer <knexer@gmail.com>
2019-02-23 12:02:32 -0800

    Add a module that jettisons stored resources.
    It isn't prioritized correctly yet, and actually it should take resources out of storage too, and be filtered, so it really isn't the right model I think.

---
+++ Assets/JettisonStoredResources.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JettisonStoredResources : MonoBehaviour
+{
+    private Module module;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        module = GetComponent<Module>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Slot storageSlot in module.OccupiedStorage)
+        {
+            Destroy(storageSlot.Occupant.gameObject);
+        }
+    }
+}
